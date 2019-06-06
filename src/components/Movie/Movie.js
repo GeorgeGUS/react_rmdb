fd1d35d4cf825cb0c510f2452b23bbda1ecb2f74
GeorgeGUS <georgegus1@gmail.com>
2019-06-06 20:32:49 +0300

    Refactor fetch promises with async/await

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -16,57 +16,44 @@ class Movie extends Component {
   };
 
   componentDidMount() {
-    const storedState = localStorage.getItem(
-      `${this.props.match.params.movieId}`
-    );
+    const { movieId } = this.props.match.params;
+    const storedState = localStorage.getItem(`${movieId}`);
     if (storedState) {
       this.setState({ ...JSON.parse(storedState) });
       return;
     }
     this.setState({ loading: true });
     // Fisrt fetch the movie...
-    const endpoint = `${API_URL}movie/${
-      this.props.match.params.movieId
-    }?api_key=${API_KEY}&language=${LANG}`;
+    const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=${LANG}`;
     this.fetchItems(endpoint);
   }
 
-  fetchItems = endpoint => {
-    fetch(endpoint)
-      .then(res => res.json())
-      .then(res => {
-        if (res.status_code) {
-          this.setState({ loading: false });
-        } else {
-          this.setState({ movie: res }, () => {
-            // ... then fetch actors in the setState cb function
-            const endpoint = `${API_URL}movie/${
-              this.props.match.params.movieId
-            }/credits?api_key=${API_KEY}`;
-            fetch(endpoint)
-              .then(res => res.json())
-              .then(res => {
-                const directors = res.crew.filter(
-                  member => member.job === 'Director'
-                );
-                this.setState(
-                  {
-                    actors: res.cast,
-                    directors,
-                    loading: false
-                  },
-                  () => {
-                    localStorage.setItem(
-                      `${this.props.match.params.movieId}`,
-                      JSON.stringify(this.state)
-                    );
-                  }
-                );
-              });
-          });
-        }
-      })
-      .catch(err => console.error('Error:', err));
+  fetchItems = async endpoint => {
+    const { movieId } = this.props.match.params;
+    try {
+      const result = await (await fetch(endpoint)).json();
+      if (result.status_code) {
+        this.setState({ loading: false });
+      } else {
+        this.setState({ movie: result });
+        // ... then fetch actors in the setState cb function
+        const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+        const { crew, cast } = await (await fetch(creditsEndpoint)).json();
+        const directors = crew.filter(member => member.job === 'Director');
+        this.setState(
+          {
+            actors: cast,
+            directors,
+            loading: false
+          },
+          () => {
+            localStorage.setItem(`${movieId}`, JSON.stringify(this.state));
+          }
+        );
+      }
+    } catch (e) {
+      console.error('Fetch error:', e);
+    }
   };
 
   render() {
@@ -83,8 +70,8 @@ class Movie extends Component {
         {actors && (
           <div className='rmdb-movie-grid'>
             <FourColGrid header={'Actors'}>
-              {actors.map(el => (
-                <Actor key={el.name} actor={el} />
+              {actors.map(actor => (
+                <Actor key={actor.name} actor={actor} />
               ))}
             </FourColGrid>
           </div>
