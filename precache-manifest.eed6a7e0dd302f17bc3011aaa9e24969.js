aeffeb32a84d2b49b20548af323effe8597ddb57
GeorgeGUS <georgegus1@gmail.com>
2019-06-29 10:25:42 +0300

    Updates

---
+++ precache-manifest.eed6a7e0dd302f17bc3011aaa9e24969.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57568c76dd773a11520e5be612589172",
+    "url": "/react_rmdb/index.html"
+  },
+  {
+    "revision": "091f16d58bf32d29d2f0",
+    "url": "/react_rmdb/static/css/main.6bfe6eaf.chunk.css"
+  },
+  {
+    "revision": "feefcbaab52f8d998757",
+    "url": "/react_rmdb/static/js/2.43877c11.chunk.js"
+  },
+  {
+    "revision": "091f16d58bf32d29d2f0",
+    "url": "/react_rmdb/static/js/main.48d3a8f5.chunk.js"
+  },
+  {
+    "revision": "610b9ca1b90245f06f0f",
+    "url": "/react_rmdb/static/js/runtime~main.f8e83255.js"
+  }
+]);
