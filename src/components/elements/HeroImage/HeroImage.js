71a21906a018668b4aeb3fb9ad1a56ffcf0c25d2
GeorgeGUS <georgegus1@gmail.com>
2019-06-30 14:26:20 +0300

    Fix HeroImage bg in Edge

--- src/components/elements/HeroImage/HeroImage.js
+++ src/components/elements/HeroImage/HeroImage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './HeroImage.css';
 
 const HeroImage = ({ image, title, text }) => {
-  const bg = {
-    backgroundImage: `linear-gradient(#0000 40%, #000a), url('${image}')`
+  const bgImage = {
+    backgroundImage: `url('${image}')`
   };
   return (
-    <div className='rmdb-heroimage' style={bg}>
+    <div className='rmdb-heroimage' style={bgImage}>
       <div className='rmdb-heroimage-content rmdb-container'>
         <div className='rmdb-heroimage-text'>
           <h1>{title}</h1>
