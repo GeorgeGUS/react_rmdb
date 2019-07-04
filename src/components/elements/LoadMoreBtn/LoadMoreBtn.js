23caa3fb08d07c79c30c412751a86b41ccba4751
GeorgeGUS <georgegus1@gmail.com>
2019-07-04 21:12:50 +0300

    Fix more btn styles and add visually-hidden class

--- src/components/elements/LoadMoreBtn/LoadMoreBtn.js
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './LoadMoreBtn.css';
 
 const LoadMoreBtn = ({ text, onClick }) => (
-  <button
-    type='button'
-    className='rmdb-loadmorebtn'
-    onClick={() => onClick(true)}
-  >
-    {text}
-  </button>
+  <div className='rmdb-container'>
+    <button
+      type='button'
+      className='rmdb-loadmorebtn'
+      onClick={() => onClick(true)}
+    >
+      {text}
+    </button>
+  </div>
 );
 
 export default LoadMoreBtn;
