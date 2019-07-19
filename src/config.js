82b1c5b0459bd36441e81873f57d9aaa823520ea
GeorgeGUS <georgegus1@gmail.com>
2019-07-19 19:20:19 +0300

    Remove custom meta tags

--- src/config.js
+++ src/config.js
@@ -7,6 +7,8 @@ const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
 const NO_IMAGE_URL = `${process.env.PUBLIC_URL}/images/no_image.jpg`;
 
+const VIDEO_URL = 'https://www.youtube.com/watch?v=';
+
 const getImageUrl = (imageSize, isBackdrop) => imagePath => {
   const noImageUrl = isBackdrop ? '' : NO_IMAGE_URL;
   return imagePath ? `${IMAGE_BASE_URL}${imageSize}${imagePath}` : noImageUrl;
@@ -20,47 +22,13 @@ const getProfileUrl = getImageUrl('w154');
 // const LANG = 'ru-RU';
 const LANG = 'en-US';
 
-const setMetaTags = (title, desc, image) => {
-  document.title = title;
-  const url = document.location.href;
-  const links = [
-    { propName: 'name', prop: 'title', content: title },
-    { propName: 'name', prop: 'description', content: desc },
-    { propName: 'property', prop: 'og:type', content: 'website' },
-    { propName: 'property', prop: 'og:url', content: url },
-    { propName: 'property', prop: 'og:title', content: title },
-    { propName: 'property', prop: 'og:description', content: desc },
-    { propName: 'property', prop: 'og:image', content: image },
-    {
-      propName: 'property',
-      prop: 'twitter:card',
-      content: 'summary_large_image'
-    },
-    { propName: 'property', prop: 'twitter:url', content: url },
-    { propName: 'property', prop: 'twitter:title', content: title },
-    { propName: 'property', prop: 'twitter:description', content: desc },
-    { propName: 'property', prop: 'twitter:image', content: image }
-  ];
-  const oldLinks = document.head.querySelectorAll('[data-mt]');
-  [...oldLinks].forEach(link => document.head.removeChild(link));
-  links.forEach(({ propName, prop, content }) => {
-    if (content) {
-      const link = document.createElement('meta');
-      link.setAttribute(propName, prop);
-      link.content = content;
-      link.dataset['mt'] = true;
-      document.head.appendChild(link);
-    }
-  });
-};
-
 export {
   API_URL,
   API_KEY,
+  VIDEO_URL,
   LANG,
   getBackdropUrl,
   getPosterUrl,
   getThumbUrl,
-  getProfileUrl,
-  setMetaTags
+  getProfileUrl
 };
