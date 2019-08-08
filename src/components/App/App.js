8596850b8898a9db67b70f13297c920c51d49e1c
GeorgeGUS <georgegus1@gmail.com>
2019-08-08 17:50:25 +0300

    Remove hash from routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ErrorBoundary from '../ErrorBoundary';
 import Header from '../elements/Header/Header';
 import NotFound from '../elements/NotFound/NotFound';
@@ -11,7 +11,7 @@ import Actor from '../pages/Actor';
 const App = () => {
   return (
     <ErrorBoundary>
-      <HashRouter>
+      <BrowserRouter basename='/react_rmdb'>
         <Header />
         <Switch>
           <Route path='/' component={Home} exact />
@@ -20,7 +20,7 @@ const App = () => {
           <Route path='/actor/:id' component={Actor} exact />
           <Route component={NotFound} />
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
     </ErrorBoundary>
   );
 };
